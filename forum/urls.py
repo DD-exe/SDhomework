3917d65ac84ee54d9185730c8392ec47306456fa
SHER <129358901+SHERAU@users.noreply.github.com>
2024-11-04 21:56:31 +0800

    Update urls.py

--- forum/urls.py
+++ forum/urls.py
@@ -3,14 +3,14 @@
 from django.conf import settings
 from django.conf.urls import include
 from django.urls import path
-from . import views
-from .views import home, post_list, new_post, delete_post, edit_post, login_view, register_view, post_detail, collect_post, remove_collect
+from UserManage import views
+from .views import home, post_list, new_post, delete_post, edit_post, post_detail, collect_post, remove_collect
 
 urlpatterns = [
     path('home/', home, name='home'),
-    path('login/', login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('register/', register_view, name='register'),
+    path('login/', views.my_login, name='login'),
+    path('logout/', views.log_out, name='logout'),
+    path('register/', views.register, name='register'),
     path('PostList/', post_list, name='post_list'),
     path('NewPost/', new_post, name='new_post'),
     path('delete/<int:post_id>/', delete_post, name='delete_post'),
