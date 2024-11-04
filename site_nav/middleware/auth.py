5b35281274af223a72bf830a63c94abce567f4f3
aiminghigh <113774525+stephenzhuCpp@users.noreply.github.com>
2024-11-05 00:13:10 +0800

    [docs] add specific and important comments for auth.py
    Request for every app.views: change `render` to `TemplateResponse`
    in order to accept rendering arguments from `site_nav.middleware.auth.InfoMiddleware`
    and `site_nav.middleware.auth.LoginMiddleware`

--- site_nav/middleware/auth.py
+++ site_nav/middleware/auth.py
@@ -16,9 +16,22 @@ import UserManage.views
 
 class InfoMiddleware:
     '''
-    对所有用户（包括未登录用户）进行普通的信息收集储存。目前主要是保存用户的上一个url。
+    对所有用户（包括未登录用户）进行普通的信息收集储存。目前主要是保存用户的上一个url和用户配置。
     
-    为了对未登录用户起作用，要在LoginMiddleware前执行。
+    “用户的上一个url”定义：
+    1. 请求的不是静态文件，即该请求将要调用各个app的视图函数;
+    2. 与当前url不同的最近的一个url。
+
+    要使用“用户的上一个url”的功能：
+    1. 场景1：在视图函数中使用。直接使用`request.session["info"]["last_url"]`，可直接redirect；
+    2. 场景2：在templates的html中使用。
+        1. 方法1（推荐）：html中：`{{ last_url }}`；views中；将html对应的函数中的`render`改为`TemplateResponse`。
+           原理：渲染参数在`InfoMiddleware.process_template_response`中被传入；
+        2. 方法2：直接从视图函数中传入html的渲染参数。
+    
+    对于视图函数返回的`TemplateResponse`添加渲染参数，从而减少html模板和视图函数的编写。
+
+    最好优先执行此中间件，以免在执行此中间件前被其他中间件拦截request。
     '''
     def __init__(self, get_response):
         self.get_response = get_response
@@ -77,9 +90,9 @@ class LoginMiddleware:
     '''
     登录中间件
 
-    判断用户是否登录，若未登录且访问了必须要登录的地址，则重定向到登录界面
+    判断用户是否登录，若未登录且访问了必须要登录的地址，则重定向到登录界面。
 
-    此外，对于视图函数返回的`TemplateResponse`添加部分登录相关的渲染参数，用来区分未登录和已登录的html界面，从而减少html模板和视图函数的编写
+    对于视图函数返回的`TemplateResponse`添加部分登录相关的渲染参数，用来区分未登录和已登录的html界面，从而减少html模板和视图函数的编写。
     '''
     # 登录可访问的地址
     loggedin_views = [views.site_nav_list, views.site_nav_add, views.site_nav_edit, views.site_nav_delete,
