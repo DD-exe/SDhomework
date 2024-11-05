8c849bc2f945c55686c18e70bee4b33b13ea94f7
SHER <129358901+SHERAU@users.noreply.github.com>
2024-11-05 10:23:30 +0800

    Update urls.py

--- forum/urls.py
+++ forum/urls.py
@@ -4,10 +4,12 @@ from django.conf import settings
 from django.conf.urls import include
 from django.urls import path
 from UserManage import views
-from .views import home, post_list, new_post, delete_post, edit_post, post_detail, collect_post, remove_collect
+from .views import home, post_list, new_post, delete_post, edit_post, post_detail, collect_post, remove_collect, new_comment, delete_comment, new_reply, delete_reply
 
 urlpatterns = [
+
     path('home/', home, name='home'),
+
     path('login/', views.my_login, name='login'),
     path('logout/', views.log_out, name='logout'),
     path('register/', views.register, name='register'),
@@ -18,7 +20,10 @@ urlpatterns = [
     path('post/<int:post_id>/', post_detail, name='post_detail'),
     path('post/<int:post_id>/collect/', collect_post, name='collect_post'),
     path('post/<int:post_id>/remove-collect/', remove_collect, name='remove_collect'),
-
+    path('post/<int:post_id>/comment/', new_comment, name='new_comment'),
+    path('comment/delete/<int:comment_id>/', delete_comment, name='delete_comment'),
+    path('post/<int:post_id>/comment/<int:comment_id>/reply/', new_reply, name='new_reply'),
+    path('reply/delete/<int:reply_id>/', delete_reply, name='delete_reply'),
 ]
 
 
