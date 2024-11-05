8992a2322c8a6bfdebff1e6806b6f3b5c2824990
SHER <129358901+SHERAU@users.noreply.github.com>
2024-11-05 11:44:21 +0800

    Update urls.py

--- forum/urls.py
+++ forum/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls import include
 from django.urls import path
 from UserManage import views
-from .views import home, post_list, new_post, delete_post, edit_post, post_detail, collect_post, remove_collect, new_comment, delete_comment, new_reply, delete_reply
+from .views import home, post_list, new_post, delete_post, edit_post, post_detail, collect_post, remove_collect, collected_posts, new_comment, delete_comment, new_reply, delete_reply
 
 urlpatterns = [
 
@@ -20,6 +20,7 @@ urlpatterns = [
     path('post/<int:post_id>/', post_detail, name='post_detail'),
     path('post/<int:post_id>/collect/', collect_post, name='collect_post'),
     path('post/<int:post_id>/remove-collect/', remove_collect, name='remove_collect'),
+    path('PostList/collected_posts/', collected_posts, name='collected_posts'),
     path('post/<int:post_id>/comment/', new_comment, name='new_comment'),
     path('comment/delete/<int:comment_id>/', delete_comment, name='delete_comment'),
     path('post/<int:post_id>/comment/<int:comment_id>/reply/', new_reply, name='new_reply'),
