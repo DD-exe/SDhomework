b49e12b0093839aa64f3f09f00329ce4362e8ca1
SHER <mengxiang0318@sjtu.edu.cn>
2024-11-06 16:08:09 +0800

    创建forum_branch分支

--- web_project/settings.py
+++ web_project/settings.py
@@ -108,9 +108,9 @@ WSGI_APPLICATION = "web_project.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'user',
+        'NAME': 'forum',
         'USER': 'root',
-        'PASSWORD': '123456789',
+        'PASSWORD': 'sher',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
