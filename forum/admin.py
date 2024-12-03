1fe8a1e9404ae8a96bfef47d0fcef28d27e43589
Bright-Crest <stephenzhu2004@163.com>
2024-12-03 17:54:06 +0800

    [fix] register models in admin.py to support django built-in admin page

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+admin.site.register(models.Subject)
+admin.site.register(models.Post)
+admin.site.register(models.Collect)
+admin.site.register(models.Comment)
+admin.site.register(models.Reply)
+
--- site_nav/admin.py
+++ site_nav/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import SiteCategory, SiteNav
+
 # Register your models here.
+
+admin.site.register(SiteCategory)
+admin.site.register(SiteNav)
+
